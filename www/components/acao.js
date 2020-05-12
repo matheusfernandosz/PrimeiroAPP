10ae88ef31934554ffa238aac4538890d4a66950
Rodrigo-Oliverira <oliver13.silva@gmail.com>
2020-05-13 05:26:26 +0900

    Adicionado os botões de Subtrair, Multiplicar e Dividir.

--- www/components/acao.js
+++ www/components/acao.js
@@ -6,3 +6,24 @@ $(document).on("click", "#btnSomar", function(){
   var resultado = parseFloat(valor1)+ parseFloat(valor2);
   $("#resultado").val(resultado);
 })
+
+$(document).on("click", "#btnSubtrair", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1)- parseFloat(valor2);
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click", "#btnMultiplicar", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1)* parseFloat(valor2);
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click", "#btnDividir", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1)/ parseFloat(valor2);
+  $("#resultado").val(resultado);
+})
